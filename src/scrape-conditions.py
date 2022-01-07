ff1bad6ee8493a4a0bfe1e94a5d15b387c0d7d8e
ImGilbes <gianlucasax@hotmail.it>
2022-01-07 13:21:49 +0100

    spent some time trying to change baseline and distances, but everything sucks, now i have to use ML

---
+++ src/scrape-conditions.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+conditions_file = "data/conditions.json"
+
+url = "https://www.nhsinform.scot/illnesses-and-conditions/a-to-z"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# print(soup)
+
+#html = soup.find() #soup.find() retrieves the first tag in the tree
+# h2 = soup.find_all('h2', {"class": "module__title"}) #in the doc conditions are h2 and class module__title
+conds = soup.find_all('a', {"class": "nhs-uk__az-link"})
+# print(conds)
+
+condlist = []
+curid = 0
+for i in conds:
+
+    # print(i.get_text().strip())
+    disease = i.get_text().strip()
+    # print(disease)
+    # condlist.append( {"id": "c"+str(curid), "name": disease, "type": disease} )
+    condlist.append( {"id": str(curid), "name": disease, "kind": disease} )
+
+    curid = curid + 1
+
+#conditions = json.dumps({ "conditions" : condlist}, indent=4)
+conditions = json.dumps(condlist, indent=4)
+
+with open(conditions_file, "w+") as file:
+    file.write(conditions)
---
+++ src/scrape-therapies.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+therapies_file = "data/therapies.json"
+
+url = "https://en.wikipedia.org/wiki/List_of_therapies"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#print(soup.prettify())
+
+ths = soup.find_all('a', attrs={'title':lambda x: "therapy" in str(x)})
+
+
+thlist = []
+thid = 0
+for i in ths:
+    curtherapy = i.get_text().strip()
+    # thlist.append( {"id": "t"+str(thid), "name": curtherapy, "type": curtherapy} )
+    thlist.append( {"id": str(thid), "name": curtherapy, "type": curtherapy} )
+    thid = thid + 1
+
+with open(therapies_file, "w+") as fp:
+    #fp.write(json.dumps( {"therapies": thlist}, indent=4))
+    fp.write(json.dumps(thlist, indent=4))
