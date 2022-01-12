21e69727a2e1beb1f396b45383cce9d35787d4f0
ImGilbes <gianlucasax@hotmail.it>
2022-01-12 17:03:52 +0100

    working svd!!! with pytorch!!!

--- src/hashing.py
+++ src/hashing.py
@@ -6,6 +6,15 @@ from numpy.core.fromnumeric import shape
 from sklearn.metrics import mean_squared_error, pairwise
 from sklearn.model_selection import train_test_split
 import sys
+from sklearn.decomposition import TruncatedSVD
+from sklearn.utils.extmath import randomized_svd
+
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+from torch.nn.modules.module import Module
+from torch.utils.data import Dataset, DataLoader
 
 datapath = "../data/patients.json"
 
@@ -128,10 +137,89 @@ def main():
     newpatient = int(sys.argv[1])
     newcond = int(sys.argv[2])
 
-    # for i in range(len(patients)):
-    #     if(len(lshUxT[UxT[i]]))> 0:
-    #         print(len(lshUxT[UxT[i]]))
-    print(len(lshUxT[UxT[newpatient]]))
+    smallerUxT = np.array( [UxT[i] for i in lshUxT[UxT[newpatient]]] )
+    print(f'LSH bucket dim={len(lshUxT[UxT[newpatient]])}, \n {smallerUxT}')
 
+
+    #input nxm
+    #nxk, k, kxm
+    u, s, vh = randomized_svd(smallerUxT, n_components=20, n_iter=2, random_state=42)
+    s = np.diag(s)
+    q = np.dot(s, vh)
+    # print(f'SVD\n{u}\n{s}\n{vh}')
+    print(f'SVD\n{u.shape}\n{s.shape}\n{vh.shape}')
+    print(f'SVD\n{u.shape}\n{q.shape}')
+
+    # x_reconstr = np.dot(u, q)
+    # nonzero_UxT = smallerUxT[smallerUxT.nonzero()]
+    # nonzero_predictions = x_reconstr[smallerUxT.nonzero()]
+    # mse = mean_squared_error(nonzero_UxT, nonzero_predictions)
+    # print(mse)
+
+    # print(smallerUxT[0],"\n",b[0])
+    # for i in range(len(therapies)):
+    #     if(smallerUxT[0][i] == 0):
+    #         print(smallerUxT[0][i],"  ",b[0][i])
+
+    num_latent_fact = 20
+  
+    
+    # p_param = torch.from_numpy(u).float()
+    # q_param = torch.from_numpy(q).float()
+
+    # p_param.requires_grad = True
+    # q_param.requires_grad = True
+    p_param = torch.randn(smallerUxT.shape[0], num_latent_fact, requires_grad=True)
+    q_param = torch.randn(num_latent_fact, smallerUxT.shape[1], requires_grad=True)
+
+    target = torch.from_numpy(smallerUxT).float()
+
+    def forward():
+        return torch.mm(p_param, q_param)
+
+    learning_rate = 1
+    n_epochs = 20000
+
+    def loss(y, y_pred):
+        return ((y_pred - y)**2).mean()
+
+    for epoch in range(n_epochs):
+
+        prediction = forward()
+        l = loss(target[target.nonzero(as_tuple=True)], prediction[target.nonzero(as_tuple=True)])
+
+        print(f'loss={l}')
+        l.backward()
+
+        with torch.no_grad():
+            p_param -= learning_rate * p_param.grad
+            q_param -= learning_rate * q_param.grad
+            # print(q_param.grad)
+            p_param.grad.zero_()
+            q_param.grad.zero_()
+
+        if epoch == 10000:
+            learning_rate = 0.1
+        if epoch == 18000:
+            learning_rate = 0.01
+        
+            
+    
+    with torch.no_grad():
+        x_hat = torch.mm(p_param, q_param).detach().numpy()
+
+    print(x_hat)
+    print("\n\n")
+    a = loss(smallerUxT[2][np.nonzero(smallerUxT[2])], x_hat[2][np.nonzero(smallerUxT[2])])
+    print(f'{smallerUxT[2][np.nonzero(smallerUxT[2])]}\n{x_hat[2][np.nonzero(smallerUxT[2])]}')
+    print(a)
+    print("\n\n")
+    a = loss(smallerUxT[5][np.nonzero(smallerUxT[5])], x_hat[5][np.nonzero(smallerUxT[5])])
+    print(f'{smallerUxT[5][np.nonzero(smallerUxT[5])]}\n{x_hat[5][np.nonzero(smallerUxT[5])]}')
+    print(a)
+    print("\n\n")
+    a = loss(smallerUxT[0][np.nonzero(smallerUxT[0])], x_hat[0][np.nonzero(smallerUxT[0])])
+    print(f'{smallerUxT[0][np.nonzero(smallerUxT[0])]}\n{x_hat[0][np.nonzero(smallerUxT[0])]}')
+    print(a)
 if __name__=="__main__":
     main()
